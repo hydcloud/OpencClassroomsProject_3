6a0c0b73a2d0cee582dae0b669e02e882883d4f4
hydcloud@gmail.com <hydcloud@gmail.com>
2026-07-24 16:28:51 +0200

    "Implémentation de la validation de la connexion utilisateur"

---
+++ backend/src/main/java/fr/datashare/backend/exception/InvalidCredentialsException.java
@@ -0,0 +1,8 @@
+package fr.datashare.backend.exception;
+
+public class InvalidCredentialsException extends RuntimeException {
+
+    public InvalidCredentialsException() {
+        super("Email ou mot de passe incorrect.");
+    }
+}
