6e9eb7e4b408135e9a83de2fa4adf8e1f8ec9248
hydcloud@gmail.com <hydcloud@gmail.com>
2026-07-24 15:00:15 +0200

    Test Commit

---
+++ backend/src/main/java/fr/datashare/backend/exception/ErrorResponse.java
@@ -0,0 +1,13 @@
+package fr.datashare.backend.exception;
+
+import java.time.LocalDateTime;
+
+public record ErrorResponse(
+
+        LocalDateTime timestamp,
+        int status,
+        String error,
+        String message
+
+) {
+}
