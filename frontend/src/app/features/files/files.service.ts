1a1309dc6076c72d14118433dfdd85662c758253
hydcloud@gmail.com <hydcloud@gmail.com>
2026-07-24 09:58:18 +0200

    Test Commit

--- frontend/src/app/features/files/files.service.ts
+++ frontend/src/app/features/files/files.service.ts
@@ -1,3 +1,7 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { environment } from '../../../environments/environment';
 @Injectable({
   providedIn: 'root'
 })
