28526687faa830d39af77964fec044f86ec14736
hydcloud@gmail.com <hydcloud@gmail.com>
2026-07-24 17:11:39 +0200

    "feat(security): implement custom user details service"

--- backend/src/main/java/fr/datashare/backend/security/CustomUserDetailsService.java
+++ backend/src/main/java/fr/datashare/backend/security/CustomUserDetailsService.java
@@ -1,4 +1,34 @@
 package fr.datashare.backend.security;
 
-public class CustomUserDetailsService {
+import fr.datashare.backend.entity.User;
+import fr.datashare.backend.repository.UserRepository;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email)
+            throws UsernameNotFoundException {
+
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException(
+                                "Utilisateur introuvable."
+                        )
+                );
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getEmail())
+                .password(user.getPassword())
+                .authorities("ROLE_USER")
+                .build();
+    }
 }
