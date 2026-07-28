6ac1666d652d803104d872ad30780e8bd1bb3725
hydcloud@gmail.com <hydcloud@gmail.com>
2026-07-28 16:54:03 +0200

    "feat(files): automatically delete expired files"

--- backend/src/main/java/fr/datashare/backend/repository/StoredFileRepository.java
+++ backend/src/main/java/fr/datashare/backend/repository/StoredFileRepository.java
@@ -4,10 +4,13 @@ import fr.datashare.backend.entity.StoredFile;
 import fr.datashare.backend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+
 import java.util.List;
 
 public interface StoredFileRepository
         extends JpaRepository<StoredFile, Long> {
 
     List<StoredFile> findAllByOwnerOrderByUploadedAtDesc(User owner);
+    List<StoredFile> findAllByExpiresAtBefore(LocalDateTime dateTime);
 }
